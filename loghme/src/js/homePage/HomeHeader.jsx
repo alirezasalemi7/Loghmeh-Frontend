6dd9b5159a7f465f4dcae88a1376a26ec11cabda
r.ghanbari <rezaqanbari1925@gmail.com>
2020-04-07 02:15:52 +0430

    Add HomeHeader component

---
+++ loghme/src/js/homePage/HomeHeader.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import "../../css/home.css"
+
+export class HomeHeader extends Component {
+    render() {
+        return (
+            <div className="backgruond-image text-center text-white">
+                <div className="header-cover h-100">
+                    <img src={require("../../assets/LOGO.png")} className="mt-4 logo-image" alt="logo"/>
+                    <p className="my-4 text-white home-top-text">اولین و بزرگ‌ترین وب‌سایت سفارش آنلاین غذا در دانشگاه تهران</p>
+                    <SearchBox></SearchBox>
+                </div>
+            </div>
+        )
+    }
+}
+
+class SearchBox extends Component {
+    render() {
+        return (
+            <div className="row justify-content-center bg-white border-0 search-box py-2 px-1 mx-auto">
+                <button type="submit" className="col-3 search-box-margin text-center border maize search-box-radius">جست‌و‌جو</button>
+                <input type="text" className="col-4 search-box-margin border search-box-radius" dir="rtl" placeholder="نام رستوران" />
+                <input type="text" className="col-4 search-box-margin border search-box-radius" dir="rtl" placeholder="نام غذا" />
+            </div>
+        )
+    }
+}
