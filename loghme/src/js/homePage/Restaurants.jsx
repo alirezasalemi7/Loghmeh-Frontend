8b4d290a20fe0975aee468c3f571f9b2ee7d107a
r.ghanbari <rezaqanbari1925@gmail.com>
2020-04-07 06:56:05 +0430

    Add restaurant header info

--- loghme/src/js/homePage/Restaurants.jsx
+++ loghme/src/js/homePage/Restaurants.jsx
@@ -39,7 +39,7 @@ export class RestaurantsContainer extends Component {
     }
 
     render() {
-        let restaurants = this.state.restaurants.map((element)=><RestaurantCart name={element.name} imageSrc={element.img} id={element.id}></RestaurantCart>)
+        let restaurants = this.state.restaurants.map((element, i)=><RestaurantCart key={i} name={element.name} imageSrc={element.img} id={element.id}></RestaurantCart>)
         return (
             <SnackBarContext>
                 <SnackBarGlobalContext.Consumer>
@@ -47,9 +47,9 @@ export class RestaurantsContainer extends Component {
                         (data) => {
                             this.show = data.showSnackbar
                             return (
-                                <div class="text-center">
-                                    <p class="part-title mx-auto mb-3">رستوران‌ها</p>
-                                    <div class="row mx-auto mb-5 restaurant-container" dir="rtl">
+                                <div className="text-center">
+                                    <p className="part-title mx-auto mb-3">رستوران‌ها</p>
+                                    <div className="row mx-auto mb-5 restaurant-container" dir="rtl">
                                         {restaurants}
                                     </div>
                                 </div>
@@ -72,11 +72,11 @@ class RestaurantCart extends Component {
 
     render() {
         return (
-            <div class="card align-items-center p-1 restaurant-card border">
-                <img class="restaurant-image rounded-circle mb-1 mt-2" src={this.props.imageSrc} alt="Restaurant Image"/>
-                <div class="card-body text-center p-0">
-                    <p class="restaurant-name-text mb-2">{this.props.name}</p>
-                    <button type="submit" class="border maize px-2 menu-button" onClick={(e)=>this.gotoRestaurantPage(this.props.id)}>نمایش منو</button>
+            <div className="card align-items-center p-1 restaurant-card border">
+                <img className="restaurant-image rounded-circle mb-1 mt-2" src={this.props.imageSrc} alt="Restaurant Image"/>
+                <div className="card-body text-center p-0">
+                    <p className="restaurant-name-text mb-2">{this.props.name}</p>
+                    <button type="submit" className="border maize px-2 menu-button" onClick={(e)=>this.gotoRestaurantPage(this.props.id)}>نمایش منو</button>
                 </div>
             </div>
         )
